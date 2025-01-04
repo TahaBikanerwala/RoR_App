827675dd097c3dcb09386fe25a3570ff666c9126
TahaBikanerwala <tahabikanerwala@gmail.com>
2025-01-04 19:05:51 +0530

    Added User model, migration and validations

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get "about"   => "static_pages#about"
   get "help"    => "static_pages#help"
   get "contact" => "static_pages#contact"
-
+  get "signup" => "users#new"
   get "up" => "rails/health#show", as: :rails_health_check
 end
---
+++ test/models/user_test.rb
@@ -0,0 +1,56 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "User should be valid" do
+    assert @user.valid?
+  end
+
+  test "User should have a name" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "User should have an email" do
+    @user.email =  "   "
+    assert_not @user.valid?
+  end
+
+  test "Name should not be too long" do
+    @user.name= "t"*21
+    assert_not @user.valid?
+  end
+
+  test "Email should not be too long" do
+    @user.name= "t"*256
+    assert_not @user.valid?
+  end
+
+  test "Email validation should be accept valid emails" do
+    valid_emails = %w[user@example.com USER@example.COM A_vA-lId@email.in.org
+                      first.last@ya.jp alice445+jo3@sad.cji]
+    valid_emails.each do |valid_email|
+      @user.email = valid_email
+      assert @user.valid?, "#{valid_email.inspect} should be valid"
+    end
+  end
+
+  test "Email valid should not accept invalid emails" do
+    invalid_emails = %w[ur@e+com USER@example,COM A_vA-lIdemail.in.org
+                      first.last@ya. alice445+jo3@sad_cji.co]
+    invalid_emails.each do |invalid_email|
+      @user.email = invalid_email
+      assert_not @user.valid?, "#{invalid_email.inspect} should be valid"
+    end
+  end
+
+  test "Email should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+end
