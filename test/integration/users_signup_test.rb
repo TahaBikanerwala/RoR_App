8a93fb6a5fdde1cb30fdb213e9597f4dfb21c1e1
TahaBikanerwala <tahabikanerwala@gmail.com>
2025-01-06 18:59:01 +0530

    Added User Signup flow and tests

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "for invalid user signup" do
+    get signup_path
+    assert_no_difference "User.count" do
+    post users_path, params: { user: { name:  "  ", email: "user@invalid",
+    password: "foo", password_confirmation: "bar" } }
+    assert_template "users/new"
+    end
+  end
+
+  test "for valid user signup" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: { name: "Test", email: "user@valid.co",
+      password: "foobar", password_confirmation: "foobar" } }
+      follow_redirect!
+      assert_template "users/show/"
+    end
+  end
+end
